ee7b3623befeb70f9b62e8bc36e9bdb75d0188fa
michalkossakowski <kossakowskimichal2002@gmail.com>
2024-03-09 15:27:18 +0100

    dodano formularz kontakotwy

---
+++ ProjektMVCdotnet8/Models/ContactModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjektMVCdotnet8.Models
+{
+    public class ContactModel
+    {
+        public string Email { get; set; }
+        public string Type { get; set; }
+        public string Topic { get; set; }
+        public string Message { get; set; }
+    }
+}

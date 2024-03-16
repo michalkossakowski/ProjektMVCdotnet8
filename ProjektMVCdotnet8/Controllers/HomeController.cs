92710e91b22b767df82cc23001c247bb786508b9
InterfectoremCubiculum <133600442+InterfectoremCubiculum@users.noreply.github.com>
2024-03-16 21:48:05 +0100

    Dodaje rekordy categorie

--- ProjektMVCdotnet8/Controllers/HomeController.cs
+++ ProjektMVCdotnet8/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using ProjektMVCdotnet8.Areas.Identity.Data;
+using ProjektMVCdotnet8.Entities;
 using ProjektMVCdotnet8.Models;
 using System.Diagnostics;
 
@@ -9,12 +11,15 @@ namespace ProjektMVCdotnet8.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
 
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger , ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
+            CreateElements();
         }
         public IActionResult Index()
         {
@@ -57,6 +62,40 @@ namespace ProjektMVCdotnet8.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async void CreateElements()
+        {
+            if (_context.Categories.IsNullOrEmpty()) //Sprawdza czy tablica jest pusta, je¿eli tak to tworzy elementy do tablicy
+            {
+                if (ModelState.IsValid)
+                {
+                    CategoryEntity category = new CategoryEntity();
+                    category.CategoryName = "Elektronika";
+                    _context.Add(category);
+
+                    category = new CategoryEntity();
+                    category.CategoryName = "Programowanie";
+                    _context.Add(category);
+
+                    category = new CategoryEntity();
+                    category.CategoryName = "Komputery";
+                    _context.Add(category);
+
+                    category = new CategoryEntity();
+                    category.CategoryName = "Sieci";
+                    _context.Add(category);
+
+                    category = new CategoryEntity();
+                    category.CategoryName = "Spawanie";
+                    _context.Add(category);
+
+                    category = new CategoryEntity();
+                    category.CategoryName = "Elektryka";
+                    _context.Add(category);
+                    await _context.SaveChangesAsync();
+                }
+            }
         }
 
     }

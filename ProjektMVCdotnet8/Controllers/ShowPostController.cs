b7811ba05bcafa208e6267040dddf1223fd8ab99
InterfectoremCubiculum <133600442+InterfectoremCubiculum@users.noreply.github.com>
2024-03-17 18:48:31 +0100

    Pokazywanie postów

--- ProjektMVCdotnet8/Controllers/ShowPostController.cs
+++ ProjektMVCdotnet8/Controllers/ShowPostController.cs
@@ -16,48 +16,11 @@ namespace ProjektMVCdotnet8.Controllers
         public ShowPostController(ApplicationDbContext context)
         {
             _context = context;
-
-            if (_context.Categories.IsNullOrEmpty()) //Sprawdza czy tablica jest pusta, jeżeli tak to tworzy elementy do tablicy
-            {
-                CreateElements();
-            }
         }
         public async Task<IActionResult> Index(string CategoryName)
         {
             var posts = _context.Posts.Where(post => post.Categories.Any(category => category.CategoryName.Equals(CategoryName))).ToList();
             return View("Index", posts);
-        }
-
-        //Rekordy testowe ----Usunąć w finalnej wersji----
-        public async void CreateElements()
-        {
-            if (ModelState.IsValid)
-            {
-                CategoryEntity category = new CategoryEntity();
-                category.CategoryName = "Elektronika";
-                _context.Add(category);
-
-                category = new CategoryEntity();
-                category.CategoryName = "Programowanie";
-                _context.Add(category);
-
-                category = new CategoryEntity();
-                category.CategoryName = "Komputery";
-                _context.Add(category);
-
-                category = new CategoryEntity();
-                category.CategoryName = "Sieci";
-                _context.Add(category);
-
-                category = new CategoryEntity();
-                category.CategoryName = "Spawanie";
-                _context.Add(category);
-
-                category = new CategoryEntity();
-                category.CategoryName = "Elektryka";
-                _context.Add(category);
-                await _context.SaveChangesAsync();
-            }
         }
     }
 }

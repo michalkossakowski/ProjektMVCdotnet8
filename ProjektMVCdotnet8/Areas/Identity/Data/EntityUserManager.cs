14d83d2a53720e55286e978dfdd6392f2f572cb1
Maison16 <mateusz.izdebski125@gmail.com>
2024-03-18 02:09:47 +0100

    dodawanie wyświetlania się nazwy użytkownika

---
+++ ProjektMVCdotnet8/Areas/Identity/Data/EntityUserManager.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using ProjektMVCdotnet8.Areas.Identity.Data;
+
+public class EntityUserManager<TUser> : UserManager<TUser> where TUser : class
+{
+
+    public EntityUserManager(IUserStore<TUser> store, IOptions<IdentityOptions> optionsAccessor, IPasswordHasher<TUser> passwordHasher, IEnumerable<IUserValidator<TUser>> userValidators, IEnumerable<IPasswordValidator<TUser>> passwordValidators, ILookupNormalizer keyNormalizer, IdentityErrorDescriber errors, IServiceProvider services, ILogger<UserManager<TUser>> logger)
+        : base(store, optionsAccessor, passwordHasher, userValidators, passwordValidators, keyNormalizer, errors, services, logger)
+    {
+    }
+
+    public string GetNick(TUser user)
+    {
+        return (user as UserEntity)?.UserName;
+    }
+}
--- ProjektMVCdotnet8/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ProjektMVCdotnet8/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -118,7 +118,7 @@ namespace ProjektMVCdotnet8.Areas.Identity.Pages.Account
             {
                 var user = CreateUser();
                 user.Nick = Input.Nick;
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                await _userStore.SetUserNameAsync(user, Input.Nick, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
 

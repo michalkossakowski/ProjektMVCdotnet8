efb4cee81b4ca131efb69c552b19b4758d13cd06
michalkossakowski <kossakowskimichal2002@gmail.com>
2024-03-23 19:54:39 +0100

    awatar przy poście

--- ProjektMVCdotnet8/Controllers/HomeController.cs
+++ ProjektMVCdotnet8/Controllers/HomeController.cs
@@ -248,7 +248,7 @@ namespace ProjektMVCdotnet8.Controllers
 
                 categories = _context.Categories.Where(x => x.CategoryName.Equals("Komputery")).ToList();
                 AttachmentSource = ("szopdzilla.jpg");
-                posts = new PostEntity(user, "Ratuj się kto może", "Świeże drożdże ocieplić w temperaturze pokojowej. Przygotować rozczyn: drożdże rozpuścić w ciepłej wodzie, dodać 2 łyżki mąki oraz cukier, dokładnie wymieszać i odstawić na ok. 10 minut do wyrośnięcia (rozczyn ze świeżych drożdży zwiększa objętość o ok. 3 razy - jeśli tak się nie stanie proces przygotowania rozczynu trzeba powtórzyć od nowa, natomiast rozczyn z drożdży instant może się tylko trochę spienić).\r\nMąkę przesiać do miski, wymieszać z solą, zrobić wgłębienie w środku i wlać w nie rozczyn. Sukcesywnie zagarniać łyżką mąkę do środka i przez 2 - 3 minuty mieszać składniki, pod koniec dodając jeszcze oliwę.\r\nPołączone składniki wyłożyć na stolnicę oprószoną mąką. Wyrabiać przez ok. 15 minut aż ciasto będzie elastyczne i gładkie (ciasto można też zagnieść mikserem planetarnym).\r\nWyrobione ciasto włożyć do dużej miski, przykryć ściereczką i odstawić na ok. 1 godzinę do wyrośnięcia.\r\nWyrośnięte ciasto wyjąć na stolnicę i chwilę pozagniatać. Podzielić na 2 części, uformować z nich kulki i odłożyć na ok. 7 minut pod ściereczką.\r\nBlaszki (tortownice) posmarować oliwą. Włożyć na środek kulkę ciasta, delikatnie spłaszczyć i rozciągać, rozprowadzając palcami po całej powierzchni dna, zaczynając od środka i zostawiając niewielki \"wałeczek\" na brzegu (zob. zdjęcia poniżej). UWAGA: najlepiej robić to kilkoma etapami, ciasto na początku sprężynuje i \"cofa się\" ale jeśli odczekamy chwilę będziemy mogli je dalej rozciągać.\r\nWyłożyć cienką warstwę SOSU POMIDOROWEGO, ser* oraz ulubione dodatki. Odczekać ok. 15 minut aż ciasto podrośnie, następnie piec w maksymalnie nagrzanym piekarniku (min. 250 st. C) przez ok. 10 minut.", AttachmentSource, categories);
+                posts = new PostEntity(user1, "Ratuj się kto może", "Świeże drożdże ocieplić w temperaturze pokojowej. Przygotować rozczyn: drożdże rozpuścić w ciepłej wodzie, dodać 2 łyżki mąki oraz cukier, dokładnie wymieszać i odstawić na ok. 10 minut do wyrośnięcia (rozczyn ze świeżych drożdży zwiększa objętość o ok. 3 razy - jeśli tak się nie stanie proces przygotowania rozczynu trzeba powtórzyć od nowa, natomiast rozczyn z drożdży instant może się tylko trochę spienić).\r\nMąkę przesiać do miski, wymieszać z solą, zrobić wgłębienie w środku i wlać w nie rozczyn. Sukcesywnie zagarniać łyżką mąkę do środka i przez 2 - 3 minuty mieszać składniki, pod koniec dodając jeszcze oliwę.\r\nPołączone składniki wyłożyć na stolnicę oprószoną mąką. Wyrabiać przez ok. 15 minut aż ciasto będzie elastyczne i gładkie (ciasto można też zagnieść mikserem planetarnym).\r\nWyrobione ciasto włożyć do dużej miski, przykryć ściereczką i odstawić na ok. 1 godzinę do wyrośnięcia.\r\nWyrośnięte ciasto wyjąć na stolnicę i chwilę pozagniatać. Podzielić na 2 części, uformować z nich kulki i odłożyć na ok. 7 minut pod ściereczką.\r\nBlaszki (tortownice) posmarować oliwą. Włożyć na środek kulkę ciasta, delikatnie spłaszczyć i rozciągać, rozprowadzając palcami po całej powierzchni dna, zaczynając od środka i zostawiając niewielki \"wałeczek\" na brzegu (zob. zdjęcia poniżej). UWAGA: najlepiej robić to kilkoma etapami, ciasto na początku sprężynuje i \"cofa się\" ale jeśli odczekamy chwilę będziemy mogli je dalej rozciągać.\r\nWyłożyć cienką warstwę SOSU POMIDOROWEGO, ser* oraz ulubione dodatki. Odczekać ok. 15 minut aż ciasto podrośnie, następnie piec w maksymalnie nagrzanym piekarniku (min. 250 st. C) przez ok. 10 minut.", AttachmentSource, categories);
                 _context.Add(posts);
 
                 ReportPostEntity reportPost2 = new ReportPostEntity();
@@ -259,7 +259,7 @@ namespace ProjektMVCdotnet8.Controllers
 
                 categories = _context.Categories.Where(x => x.CategoryName.Equals("Komputery")).ToList();
                 AttachmentSource = ("szop_potter.jpg");
-                posts = new PostEntity(user, "Tylko nie sliterin!!!", "szop który był wybrańcem", AttachmentSource, categories);
+                posts = new PostEntity(user2, "Tylko nie sliterin!!!", "szop który był wybrańcem", AttachmentSource, categories);
                 _context.Add(posts);
 
                 categories = _context.Categories.Where(x => x.CategoryName.Equals("Komputery") || x.CategoryName.Equals("Sieci")).ToList();
@@ -269,12 +269,12 @@ namespace ProjektMVCdotnet8.Controllers
 
                 categories = _context.Categories.Where(x => x.CategoryName.Equals("Elektryka") || x.CategoryName.Equals("Komputery")).ToList();
                 AttachmentSource = ("szop_batman.jpg");
-                posts = new PostEntity(user, "Something in the way", "zło nigdy nie śpi", AttachmentSource, categories);
+                posts = new PostEntity(user1, "Something in the way", "zło nigdy nie śpi", AttachmentSource, categories);
                 _context.Add(posts);
 
                 categories = _context.Categories.Where(x => x.CategoryName.Equals("Elektronika") || x.CategoryName.Equals("Komputery") || x.CategoryName.Equals("Sieci")).ToList();
                 AttachmentSource = ("szop_szambo.jpg");
-                posts = new PostEntity(user, "Szambo Nurek", "zagrożony wygnięciem. Szambo nuerk najmniejszy przerzuwać świata", AttachmentSource, categories);
+                posts = new PostEntity(user2, "Szambo Nurek", "zagrożony wygnięciem. Szambo nuerk najmniejszy przerzuwać świata", AttachmentSource, categories);
                 _context.Add(posts);
 
                 categories = _context.Categories.Where(x => x.CategoryName.Equals("Komputery") || x.CategoryName.Equals("Sieci")).ToList();
@@ -284,7 +284,7 @@ namespace ProjektMVCdotnet8.Controllers
 
                 categories = _context.Categories.Where(x => x.CategoryName.Equals("Komputery")).ToList();
                 AttachmentSource = ("szop_skywalker.jpg");
-                posts = new PostEntity(user, "Nowa nadzije", "Nastały czasy wojny domowej. Statki Rebeliantów, atakujące z ukrytej bazy, odniosły pierwsze zwycięstwo w walce ze złowrogim Imperium Galaktycznym.\r\n\r\nSzpiedzy wykradli tajne plany ostatecznej broni Imperium, GWIAZDY SMIERCI, stacji kosmicznej o sile rażenia zdolnej zniszczyć całą planetę.\r\n\r\nŚcigana przez agentów Imperium, Księżniczka Leia ucieka do domu, strzegąc wykradzionych planów, które mogą ocalić jej lud i przywrócić wolność galaktyce.", AttachmentSource, categories);
+                posts = new PostEntity(user1, "Nowa nadzije", "Nastały czasy wojny domowej. Statki Rebeliantów, atakujące z ukrytej bazy, odniosły pierwsze zwycięstwo w walce ze złowrogim Imperium Galaktycznym.\r\n\r\nSzpiedzy wykradli tajne plany ostatecznej broni Imperium, GWIAZDY SMIERCI, stacji kosmicznej o sile rażenia zdolnej zniszczyć całą planetę.\r\n\r\nŚcigana przez agentów Imperium, Księżniczka Leia ucieka do domu, strzegąc wykradzionych planów, które mogą ocalić jej lud i przywrócić wolność galaktyce.", AttachmentSource, categories);
                 _context.Add(posts);
 
                 _context.SaveChanges();

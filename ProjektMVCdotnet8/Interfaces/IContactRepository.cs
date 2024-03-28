c8ba67671a57a12e76c2a16725006015cda732c8
michalkossakowski <kossakowskimichal2002@gmail.com>
2024-03-28 22:42:43 +0100

    interfejs contact ale nie mamy gdzie go użyć

---
+++ ProjektMVCdotnet8/Interfaces/IContactRepository.cs
@@ -0,0 +1,15 @@
+﻿using ProjektMVCdotnet8.Entities;
+
+namespace ProjektMVCdotnet8.Interfaces
+{
+    public interface IContactRepository
+    {
+        Task<IEnumerable<ContactEntity>> GetAll();
+        Task<ContactEntity> GetByIdAsync(int id);
+        bool Add(ContactEntity contact);
+        bool Delete(ContactEntity contact);
+        bool Delete(int id);
+        bool Update(ContactEntity contact);
+        bool Save();
+    }
+}

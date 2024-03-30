52781846b715440b458b8aca2e63d7706835e625
michalkossakowski <kossakowskimichal2002@gmail.com>
2024-03-30 20:50:07 +0100

    teraz reporty są obsługiwane przez report repositopry i post repository bez context

---
+++ ProjektMVCdotnet8/Repository/ReportRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjektMVCdotnet8.Areas.Identity.Data;
+using ProjektMVCdotnet8.Entities;
+using ProjektMVCdotnet8.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ProjektMVCdotnet8.Repository
+{
+    public class ReportRepository : IReportRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReportRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool Add(ReportPostEntity report)
+        {
+            _context.Add(report);
+            return Save();
+        }
+
+        public bool Delete(ReportPostEntity report)
+        {
+            _context.Remove(report);
+            return Save();
+        }
+
+        public bool DeleteById(int id)
+        {
+            var report = _context.ReportPosts.Find(id);
+            _context.ReportPosts.Remove(report);
+            return Save();
+        }
+
+        public async Task<IEnumerable<ReportPostEntity>> GetAll()
+        {
+            return await _context.ReportPosts.ToListAsync();
+        }
+
+        public async Task<ReportPostEntity> GetById(int id)
+        {
+            return await _context.ReportPosts.FindAsync(id);
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0;
+        }
+    }
+}
